e6e50951e77ec2c968ed53a49245ea2b838f6be1
hbheng <126035036+hbheng@users.noreply.github.com>
2023-03-23 15:22:23 +0700

    Add files via upload

---
+++ Bonus.java
@@ -0,0 +1,45 @@
+package TP03;
+
+import java.util.Scanner;
+
+public class Bonus {
+    public static void main(String[] args) {
+
+        int t = 3;
+        while (t >= 1) {
+            System.out.print("""
+
+                    -------------Menu-------------
+                    1.  Second to Time
+                    2.  Time ti Second
+                    3.  Calling Cost
+                    4.  Riels to Dollar
+                    5.  Traveling Duration
+                    0.  Exit
+                    -------------------------------
+                    Choose an option:""");
+            Scanner cs = new Scanner(System.in);
+            int n = cs.nextInt();
+            switch (n) {
+                case 0:
+                    t = 0;
+                    break;
+                case 1:
+                    ex01.main(args);
+                    break;
+                case 2:
+                    ex02.main(args);
+                    break;
+                case 3:
+                    ex03.main(args);
+                    break;
+                case 4:
+                    ex04.main(args);
+                    break;
+                case 5:
+                    ex05.main(args);
+                    break;
+            }
+        }
+    }
+}
